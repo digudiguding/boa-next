a55d252549fe52590335c4dfecab73e291aebe83
digudiguding <98702501+digudiguding@users.noreply.github.com>
2023-05-11 15:03:30 +0800

    input page

---
+++ app/input/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import Layout from '../../components/Layout'
+
+export default function InputPage() {
+    return (
+      <Layout>
+      <Typography variant="h2" pt={5} align={'center'} gutterBottom> 
+        Input
+      </Typography>
+
+      </Layout>
+    )
+  }
+  
