53c23235c76af62343fc740111e6089bf6b323cc
Lee Bo Hua <bohua12@gmail.com>
2023-05-11 14:31:11 +0800

    Refactored code & integrated with dashboard

--- app/mkt/page.tsx
+++ app/mkt/page.tsx
@@ -1,16 +1,88 @@
 "use client"
-
+import { useEffect, useState } from 'react';
 import * as React from 'react';
 import { Typography } from '@mui/material';
 import Layout from '../../components/Layout'
 
 export default function MktProducer() {
+    const [jsonData, setJsonData] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [displayedEntries, setDisplayedEntries] = useState([]);
+  
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/dataproducer');
+      const data = await response.json();
+      setJsonData(data);
+    };
+    fetchData();
+  }, []);
+
+  
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDisplayedEntries(entries => {
+        if (jsonData && entries.length === jsonData.length) {
+          clearInterval(interval);
+          return entries;
+        }
+        return entries.concat(jsonData.slice(entries.length, entries.length + 1));
+      });
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [jsonData]);
+
+  useEffect(() => {
+    if (jsonData && jsonData.length > 0) {
+      setDisplayedEntries(entries => entries.concat(jsonData.slice(0, currentIndex)));
+    }
+  }, [jsonData, currentIndex]);
+
+  const currentEntry = jsonData && jsonData[currentIndex];
     return (
+      <div> 
       <Layout>
       <Typography variant="h2" pt={5} align={'center'} gutterBottom> 
       Market Data Producer 
       </Typography>
+      
       </Layout>
+
+      {displayedEntries.map(entry => (
+        <div key={entry.EventId}>
+          {entry.EventType === 'ConfigEvent' && (
+            <div>
+              <p>EventId: {entry.EventId}</p>
+              <p>EventType: {entry.EventType}</p>
+              <p>m: {entry.m}</p>
+              <p>b: {entry.b}</p>
+              <p>DivisorRatio: {entry.DivisorRatio}</p>
+              <p>Spread: {entry.Spread}</p>
+              <p>-------------------------</p>
+            </div>
+          )}
+          {entry.EventType === 'FXMidEvent' && (
+            <div>
+
+              <p>EventId: {entry.EventId}</p>
+              <p>EventType: {entry.EventType}</p>
+              <p>Ccy: {entry.Ccy}</p>
+              <p>rate: {entry.rate}</p>
+
+              <p>-------------------------</p>
+            </div>
+          )}
+
+        </div>
+      ))}
+      
+      </div>
+
+
     )
   }
   
--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -1,16 +1,73 @@
 "use client"
-
+import { useEffect, useState } from 'react';
 import * as React from 'react';
 import { Typography } from '@mui/material';
 import Layout from '../../components/Layout'
 
 export default function TradeProducer() {
+  const [jsonData, setJsonData] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [displayedEntries, setDisplayedEntries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/dataproducer');
+      const data = await response.json();
+      setJsonData(data);
+    };
+    fetchData();
+  }, []);
+
+  
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDisplayedEntries(entries => {
+        if (entries.length === jsonData.length) {
+          clearInterval(interval);
+          return entries;
+        }
+        return entries.concat(jsonData.slice(entries.length, entries.length + 1));
+      });
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [jsonData]);
+
+  useEffect(() => {
+    if (jsonData && jsonData.length > 0) {
+      setDisplayedEntries(entries => entries.concat(jsonData.slice(0, currentIndex)));
+    }
+  }, [jsonData, currentIndex]);
+
+  const currentEntry = jsonData && jsonData[currentIndex];
+
+
     return (
+      <div>
       <Layout>
       <Typography variant="h2" pt={5} align={'center'} gutterBottom> 
       Trade Event Data Producer 
       </Typography>
       </Layout>
+      {displayedEntries.map(entry => (
+        <div key={entry.EventId}>
+          {entry.EventType === 'TradeEvent' && (
+            <div>
+              <p>EventId: {entry.EventId}</p>
+              <p>EventType: {entry.EventType}</p>
+              <p>Ccy: {entry.Ccy}</p>
+              <p>BuySell: {entry.BuySell}</p>
+              <p>Tenor: {entry.Tenor}</p>
+              <p>Quantity: {entry.Quantity}</p>
+              <p>TradeId: {entry.TradeId}</p>
+              <p>-------------------------</p>
+            </div>
+          )}
+
+        </div>
+      ))}
+      </div>
+
     )
   }
   
